ac3693a39073c512e04ee2c50c2da7f8b0260eaa
viniciuslettieri <viniciuslettieri@yahoo.com.br>
2022-04-15 15:24:19 -0300

    pequenas alterações

--- Lab 1/passivo.py
+++ Lab 1/passivo.py
@@ -10,12 +10,10 @@ novo_sock, endereco = sock.accept()
 
 while True:
     msg = novo_sock.recv(1024)
-    
     if not msg: 
         break
     else:
         print(str(msg, encoding='utf-8'))
-
     novo_sock.send(msg)
 
 novo_sock.close()
