0d02fd313a3927ec0c243a2610d1182507e38855
viniciuslettieri <viniciuslettieri@yahoo.com.br>
2022-05-03 11:53:06 -0300

    corrigindo código

--- Lab 3/atividade3_server.py
+++ Lab 3/atividade3_server.py
@@ -10,28 +10,30 @@ HOST = ''
 PORT = 5000
 
 entradas = [sys.stdin]
+clientes = [] 
 
 
-def inicializaServidor():
-    """ Cria o socket passivo para o servidor """
+def inicializa_servidor():
+    """ Cria e retorna o socket passivo para o servidor """
 
     sock = socket.socket()   # default: socket.AF_INET, socket.SOCK_STREAM
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((HOST, PORT))
     sock.listen(5) 
     sock.setblocking(False)
 
     entradas.append(sock)
 
-    print(f"(Servidor) O servidor foi inicializado na porta {PORT}.")
+    print(f"(Servidor) O servidor foi inicializado na porta { PORT }.")
 
     return sock
 
 
-def aceitaConexao(sock):
+def aceita_conexao(sock):
     """ Realiza a conexão com o cliente e devolve o novo socket direto """
 
     client_sock, client_addr = sock.accept()
-    print(f"(Servidor) O servidor aceitou a conexão de ({client_sock}, {client_addr}).")
+    print(f"(Servidor) O servidor aceitou a conexão de { client_addr }.")
     return client_sock, client_addr
 
 
@@ -58,37 +60,39 @@ def atendeRequisicao(client_sock, client_addr):
 
                 client_sock.send(response.encode("utf-8"))
         except:
-            filename = str(filename, encoding="utf-8")
-            erro = f"ERRO: Arquivo '{filename}' não encontrado!".encode("utf-8")
+            erro = f"ERRO: Arquivo '{ filename.decode('utf-8') }' não encontrado!".encode("utf-8")
             client_sock.send(erro)
 
-        print(f"(Servidor) O servidor atendeu a requisição de ({client_sock}, {client_addr}).")
+        print(f"(Servidor) O cliente { client_addr } solicitou o arquivo '{ filename.decode('utf-8') }'.")
 
-    print(f"(Servidor) O cliente ({client_sock}, {client_addr}) encerrou a conexão.")
+
+    print(f"(Servidor) O cliente { client_addr } encerrou a conexão.")
     client_sock.close()
 
+def atende_stdin(server_sock):
+    comando = input()
+    print(f"(Servidor) Atendendo o input '{comando}' do stdin.")
+    
+    if comando == "exit":
+        for client in clientes:
+            client.join()
+        server_sock.close()
+        sys.exit()
 
-def main():
-    clientes = []     
-
-    server_sock = inicializaServidor()
+def main():  
+    server_sock = inicializa_servidor()
     while True:
         leitura, _, _ = select.select(entradas, [], [])
 
         for leitura_pronto in leitura:
             if leitura_pronto == server_sock:
-                client_sock, client_addr = aceitaConexao(server_sock)
-                cliente = threading.Thread(target=atendeRequisicao, args=(client_sock, client_addr))   
-                cliente.start()
-                clientes.append(cliente) 
-                client_sock.close()
+                client_sock, client_addr = aceita_conexao(server_sock)
+                thread_client = threading.Thread(target=atendeRequisicao, args=(client_sock, client_addr))   
+                thread_client.start()
+                clientes.append(thread_client) 
+                
             elif leitura_pronto == sys.stdin:
-                comando = input()
-                if comando == "exit":
-                    for client in clientes:
-                        client.join()
-                    server_sock.close()
-                    sys.exit()
+                atende_stdin(server_sock)
 
 
 main()
