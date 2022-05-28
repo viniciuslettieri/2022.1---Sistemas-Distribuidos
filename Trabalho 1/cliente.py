1d04879db11afa508d9e7c9ac0f2244c47a54d30
Darkos32 <jorgeoliv32@gmail.com>
2022-05-28 14:55:06 -0300

    comandos cliente-servidors

--- Trabalho 1/cliente.py
+++ Trabalho 1/cliente.py
@@ -1,34 +1,66 @@
-import socket
 
+import json
+import socket
+from traceback import print_tb
+import Utils
 HOST = 'localhost'  # ip/server to send messages. If it's in another computer, input the ip
 DOOR = 5000        # Door used by both client/server
 
 MESSAGE_SIZE = 256 # We will use one unsigned byte to represent size of message. 1 byte for length of message, and 2^8 - 1 for message.
+conections = []#array with the current open connectios
 
-# create socket (instantiation)
-activeSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def createConnection(host, door):
 
-# Connect with client server side
-activeSock.connect((HOST, DOOR))
+    # create socket (instantiation)
+    activeSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Keep sending message to server
-userInput = input()
+    # Connect with client server side
+    activeSock.connect((host, door))
+    return activeSock
+#Parses a command made by the user so the server can understand it
+socketServidor = createConnection(HOST,DOOR) #initialize a connection with the central server    
+def parseUserCommand(userInput):
+    parsedCommand = ''
+    
+    if(userInput == "get_lista"):
+        
+        parsedCommand = "{'operacao':'get_lista'}"
+    elif(userInput == "login"):
+        username  = userInput.split(" ")[1]
+        port = input.split(" ")[2]
+        parsedCommand = "{'operacao':'login','username':{user},'porta':{porta}}".format(user = username, porta = port)
+    else:
+        username  = userInput.split(" ")[1]
+        parsedCommand = "{'operacao':'login','username':{user}}".format(user = username)
+    return parsedCommand
+def handleServerRequest(userInput):
+    parsedInput = parseUserCommand(userInput)
+    menssage = Utils.constroi_mensagem( parsedInput)
+    socketServidor.sendall(menssage)
+    message = Utils.reconstroi_mensagem(socketServidor)
+    print(message)
+    
 
-while (userInput != "exit"):  # exit will close communication
-    message_length_byte = len(userInput).to_bytes(1, 'little', )
-    encodedMessage = message_length_byte
-    encodedMessage += bytes(userInput, encoding='utf-8')
-    activeSock.sendall(encodedMessage)
 
-    # application blocked until receives message
-    message = activeSock.recv(MESSAGE_SIZE)
+#Deals with the different possible inputs passed by the user
+def handleUserInput(userInput):
+    isForServer = 1 if (userInput =="get_lista" or userInput =="login" or userInput =="logoff") else 0
+    if(isForServer):
+        handleServerRequest(userInput)
+        return
+    
 
-    # Print received message
-    print(str(message, encoding='utf-8'))
-
-    # Keep receiving user input
+def main():
+   
+    # Keep sending message to server
     userInput = input()
 
+    while (userInput != "exit"):  # exit will close communication
+        handleUserInput(userInput)
+        # Keep receiving user input
+        userInput = input()
 
-# Close connection
-activeSock.close()
+
+    # Close connection
+    socketServidor.close()
+main()
