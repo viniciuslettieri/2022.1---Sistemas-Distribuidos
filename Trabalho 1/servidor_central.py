4fb801452670ef39db3ff755978fe1381161f6f1
thierry <thierrydutoit123@gmail.com>
2022-05-28 11:18:58 -0300

    ajustando servidor central para get_lista

--- Trabalho 1/servidor_central.py
+++ Trabalho 1/servidor_central.py
@@ -8,13 +8,15 @@ import threading
 import sys
 
 HOST = ''          # Any address will be able to reach server side
-DOOR = 5000        # Door used by both client/server
+DOOR = 5000      # Door used by both client/server
 
-MESSAGE_SIZE = 2048
+MESSAGE_SIZE = 256 # We will use one unsigned byte to represent size of message. 1 byte for length of message, and 2^8 - 1 for message.
 MAX_CONNECTIONS = 5
 DICT_RETURN_SIZE = 5
 
 inputs = [sys.stdin]
+
+connections = {'luanzinho32': {'Endereco': '10.10.10.10', 'Porta': '5000'}}
 
 
 def createServerConnection():
@@ -33,35 +35,6 @@ def createServerConnection():
 
     return sock
 
-
-def requisition(newSock, address):
-    while True:
-        # Keep blocked until receives message from client side
-        message = newSock.recv(MESSAGE_SIZE)
-        print(message)
-        # If client side doesn't send a message end communication
-        if not message:
-            print(str(address) + '-> ended')
-            newSock.close()  # encerra a conexao com o cliente
-            return
-        else:
-            print("Message received from (" + str(address[1]) + "): " +
-                  str(message, encoding='utf-8'))
-
-            file_name = str(message, encoding='utf-8')
-
-            try:
-                file = data_acess(file_name)
-
-                if (file):
-                    ans = data_process(file)
-                    # Send the same message received to client side
-                    newSock.send(
-                        bytes("Most common words from file: " + json.dumps(dict(ans)), encoding='utf-8'))
-            except Exception as error:
-                newSock.send(bytes(str(error), encoding='utf-8'))
-
-
 def interface():
 
     threads = []
@@ -70,7 +43,6 @@ def interface():
     print("Accepting Connections...")
 
     while True:
-        print(inputs)
         r, escrita, excecao = select.select(inputs, [], [])
 
         for ready in r:
@@ -91,35 +63,44 @@ def interface():
                     passiveSock.close()
                     sys.exit()
 
+def requisition(newSock, address):
+    while True:
+        # Keep blocked until receives message from client side
+        message = newSock.recv(MESSAGE_SIZE)
+        print(message)
+        print(int.from_bytes(message[0:1], 'little'))
+        # If client side doesn't send a message end communication
+        if not message:
+            print(str(address) + '-> ended')
+            newSock.close()  # encerra a conexao com o cliente
+            return
+        else:
+            print("Message received from (" + str(address[1]) + "): " +
+                  str(message[1:], encoding='utf-8'))
 
-def data_acess(file_name):
-    try:
-        file = open(file_name, "r")
-        return file
-    except FileNotFoundError as ferror:
-        raise FileNotFoundError(ferror)
+            command = str(message[1:], encoding='utf-8')
+
+            try:
+                answer = data_acess(command, address)
+
+                if (answer):
+                    # Send the same message received to client side
+                    newSock.send(
+                        bytes(answer, encoding='utf-8'))
+            except Exception as error:
+                newSock.send(bytes(str(error), encoding='utf-8'))
 
 
-def data_process(file):
-    text = file.read()
+def data_acess(command, address):
+    if (command == "get_lista"):
+        return get_lista(address) 
+    else:
+        raise ModuleNotFoundError()    
 
-    word_dict = {}
-    for word in text.split():
-        if not (word_dict.get(word)):
-            word_dict[word] = 0
-        word_dict[word] += 1
-
-    sorted_dict = dict(
-        sorted(word_dict.items(), key=operator.itemgetter(1), reverse=True))
-
-    ans = []
-    counter = 0
-    for pair in sorted_dict.items():
-        ans.append(pair)
-        counter += 1
-        if (counter == DICT_RETURN_SIZE):
-            break
-    return ans
+def get_lista(address):
+    json_string = {"operacao": "get_lista", "status": str(200), "clientes": connections, "usuario": {"endereco": str(address[0]), "porta": str(DOOR)}}
+    answer = json.dumps(json_string)
+    return answer
 
 
 def main():
